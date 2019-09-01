1ed0b18d7f20b6d3da8b23d7f3806ef343745ad4
shiho ishitoko <cidermitaina@gmail.com>
2019-09-01 23:40:33 +0900

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# cidermitaina.github.io
-https://cidermitaina.github.io
---
+++ precache-manifest.68cbef8704ce3f68d7e58d13b5de5e14.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e039891c0bac30799682b0ed8a67035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dc0ac04d6b919fcd27c",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "b074de6f2ec93187f7cb",
+    "url": "/static/css/main.ff34ee11.chunk.css"
+  },
+  {
+    "revision": "9dc0ac04d6b919fcd27c",
+    "url": "/static/js/2.5b0feec2.chunk.js"
+  },
+  {
+    "revision": "b074de6f2ec93187f7cb",
+    "url": "/static/js/main.5d288d85.chunk.js"
+  },
+  {
+    "revision": "8f41f498dbfcd0c3d86c",
+    "url": "/static/js/runtime~main.349bb476.js"
+  },
+  {
+    "revision": "437cf4f79b5153807987a6d1d08055f8",
+    "url": "/static/media/cidermitaina.437cf4f7.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
